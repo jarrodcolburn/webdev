2d2103a3fe410dbfb4ba51c0701001d6a0e16829
Jarrod Colburn <jarrod@jarrodcolburn.com>
2024-04-17 16:51:43 -0500

    WIP: rewrite fetch, still need JSON

--- dwds/debug_extension_mv3/web/web_api.dart
+++ dwds/debug_extension_mv3/web/web_api.dart
@@ -1,39 +1,43 @@
 // Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-import 'dart:html';
-import 'dart:js_util' as js_util;
+// import 'dart:html';
+// import 'dart:js_util' as js_util;
+import 'dart:js_interop';
 
-import 'package:js/js.dart';
+import 'package:web/web.dart' as web;
+// import 'package:web/' as web;
+// export 'package:web/web.dart' show fetch;
 
-@JS()
+// import 'package:js/js.dart';
+
+// @JS()
 // ignore: non_constant_identifier_names
-external Json get JSON;
+// external Json get JSON;
 
-@JS()
-@anonymous
-class Json {
+// @JS()
+// @anonymous
+// class Json {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
-  external String stringify(o);
-}
+// external String stringify(o);
+// }
 
 // Custom implementation of Fetch API until the Dart implementation supports
 // credentials. See https://github.com/dart-lang/http/issues/595.
-@JS('fetch')
-external Object _nativeJsFetch(String resourceUrl, FetchOptions options);
+// @JS('fetch')
+// external Object _nativeJsFetch(String resourceUrl, FetchOptions options);
+typedef FetchResponse = web.Response;
+typedef FetchOptions = web.RequestInit;
 
 Future<FetchResponse> fetchRequest(String resourceUrl) async {
   try {
-    final options = FetchOptions(
-      method: 'GET',
-      credentials: 'include',
-    );
-    final response =
-        await promiseToFuture(_nativeJsFetch(resourceUrl, options));
-    final body =
-        await promiseToFuture(js_util.callMethod(response, 'text', []));
-    final ok = js_util.getProperty<bool>(response, 'ok');
-    final status = js_util.getProperty<int>(response, 'status');
+    final options = FetchOptions(method: 'GET', credentials: 'include');
+    final response = await web.window.fetch(resourceUrl.toJS, options).toDart;
+    // final response = await promiseToFuture(_nativeJsFetch(resourceUrl, options));
+    final body = response.body;
+    // final body = await promiseToFuture(js_util.callMethod(response, 'text', []));
+    // final ok = js_util.getProperty<bool>(response, 'ok');
+    // final status = js_util.getProperty<int>(response, 'status');
     return FetchResponse(status: status, ok: ok, body: body);
   } catch (error) {
     return FetchResponse(
@@ -44,23 +48,23 @@ Future<FetchResponse> fetchRequest(String resourceUrl) async {
   }
 }
 
-@JS()
-@anonymous
-class FetchOptions {
-  external factory FetchOptions({
-    required String method, // e.g., 'GET', 'POST'
-    required String credentials, // e.g., 'omit', 'same-origin', 'include'
-  });
-}
+// @JS()
+// @anonymous
+// class FetchOptions {
+//   external factory FetchOptions({
+//     required String method, // e.g., 'GET', 'POST'
+//     required String credentials, // e.g., 'omit', 'same-origin', 'include'
+//   });
+// }
 
-class FetchResponse {
-  final int status;
-  final bool ok;
-  final String? body;
+// class FetchResponse {
+//   final int status;
+//   final bool ok;
+//   final String? body;
 
-  FetchResponse({
-    required this.status,
-    required this.ok,
-    required this.body,
-  });
-}
+//   FetchResponse({
+//     required this.status,
+//     required this.ok,
+//     required this.body,
+//   });
+// }
